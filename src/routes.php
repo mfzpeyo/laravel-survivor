9eb3547d035e01dc0cd4f08029ef27cec693fa49
mfzpeyo@gmail.com <mfzpeyo@gmail.com>
2023-08-10 16:17:24 +0800

    src\routes.php

--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,5 @@ use Peyotest\LaravelSurvivor\SurvivorController;
 Route::group(['middleware' => ['web']], function () {
     Route::get((config('survivor.ping_url') ?: 'survivor/ping'),
 		[SurvivorController::class,'ping']
-    ])->name('survivor.ping');
+    )->name('survivor.ping');
 });
